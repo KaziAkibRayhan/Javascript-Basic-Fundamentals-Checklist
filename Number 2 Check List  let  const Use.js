5732861e6e523bcaaaf75c3d07418ea9566778bc
Akib Rayhan <kaziakibrayhan2018@gmail.com>
2022-08-20 23:21:33 +0600

    number 2 check list

---
+++ Number 2 Check List  let  const Use.js
@@ -0,0 +1,6 @@
+let productQuantity = 34;
+    productQuantity = 32; // Change able
+    console.log(productQuantity);
+const PI = 3.1416;
+    PI = 3.667;  //Not Change able
+    console.log(PI);
--- check-list.js
+++ check-list.js
@@ -1,12 +1,4 @@
 
-// Number 2 Check List  let , const use
-
-let productQuantity = 34;
-    productQuantity = 32; // Change able
-    // console.log(productQuantity);
-const PI = 3.1416;
-    // PI = 3.667;  Not Change able
-
 // Number 3 Check List Arithmatic Operator
 
 const number1 = 3;
