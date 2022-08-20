eaeede9252c08380eea9241a4832043c7d862ea1
Akib Rayhan <kaziakibrayhan2018@gmail.com>
2022-08-20 16:37:31 +0600

    basic javascript

---
+++ check-list.js
@@ -0,0 +1,71 @@
+// Number 1 Check List Variable Decleare
+
+const myName = 'AKib Rayhan';
+const isMonday = true;
+let yourAge = 22;
+
+// Number 2 Check List  let , const use
+
+let productQuantity = 34;
+    productQuantity = 32; // Change able
+    // console.log(productQuantity);
+const PI = 3.1416;
+    // PI = 3.667;  Not Change able
+
+// Number 3 Check List Arithmatic Operator
+
+const number1 = 3;
+const number2 = 2;
+
+const add = number1 + number2;
+
+const sub = number2 - number1;
+
+const multi = number1 * number2;
+
+const div = number2 / number1;
+
+const modulus = number1 % number2;
+
+// console.log(add,sub,multi,div,modulus);
+
+// Number 4 Check List Comparision Operator
+
+const waterTank1 = 30;
+const waterTank2 = 20;
+
+ /* console.log(waterTank1 > waterTank2); true greater than
+ console.log(waterTank1 < waterTank2);  false smaller than
+ console.log(waterTank1 >= waterTank2); true greater than or equal
+ console.log(waterTank1 <= waterTank2); false smaller than or equal
+ console.log(waterTank1 != waterTank2); true দুইটা সমান না
+ console.log(waterTank1 == waterTank2); false সমান নয় । */
+
+ // Number 5 Check List ২ টা শর্ত পূরন বা যেকোনো ১ টা পূরন করতে হবে।
+
+const webDevloper = false;
+const js = false;
+const reactJs = false;
+
+ if (webDevloper && js) {
+    console.log("you are devloper");
+ }
+
+ else if (reactJs || js) {
+    console.log('you are fronted developer');
+ }
+
+ else{
+    console.log('you are not developer');
+ }
+
+
+
+
+
+
+
+
+
+
+
